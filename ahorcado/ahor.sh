2a438435c58d837b42dea9542f70a47821e5db7c
netosaurio11 <neto1014@gmail.com>
2017-02-26 00:01:27 -0600

    Se ponen algunas especificaciones en el juego al pedir la palabra secreta

--- ahorcado/ahor.sh
+++ ahorcado/ahor.sh
@@ -16,7 +16,11 @@ while [[ j -eq 1 ]]; do
 limpiar
 cat bienvenido.txt;
 
-read -p "Jugador 1 ingresa la palabra: " -s palabra                # Se lee la palabra
+echo;echo;echo;
+echo "Ingresa UNA palabra SIN espacios"
+echo;echo;echo;
+
+read -p "Jugador 1 ingresa la palabra(NO TE ESPANTES SI NO VES NADA, ES NORMAL, SI ESTAS ESCRIBIENDO): " -s palabra                # Se lee la palabra
 let longitud=`expr length "$palabra"`-1     # Sacar la longitud de la palabra:
 # echo $longitud
 declare -a adivinar[$longitud]
