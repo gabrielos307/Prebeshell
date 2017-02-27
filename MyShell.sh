2f60645ac2f7e042f74cfb61b604a41fe80d08fa
rodrigocastillo1 <castillo_15zs@hotmail.com>
2017-02-26 21:57:06 -0800

    Se añade la función para poder usar comandos nativos en la prbeshell

--- MyShell.sh
+++ MyShell.sh
@@ -2,12 +2,13 @@
 
 trap "" 2 20
 trap "" SIGTSTP  # Ctrl+z y Ctrl+c
+ruta_actual=$PWD 
 
 MyShell()
 {
     clear
     echo "";
-    cat bienvenida.txt
+    cat bienvenida.txt; 
     echo "";
     echo -e "                          \e[1;34mLista de comandos: ";
     echo "";
@@ -23,33 +24,79 @@ MyShell()
     echo "                              salir";
     echo "";
 
-    echo -e "\e[0;32mSuperSimon\e[0;31m@\e[0;36m`whoami`\e[0;32m$\e[0;33m\c"; 
-    read -p "Dice: " menuPrincipal
+    echo -e "\e[0;32mSuperSimon\e[0;31m@\e[0;36m`whoami``pwd`\e[0;32m$\e[0;33m\c"; 
+    read -p "Dice: " comando
 
 
-    case $menuPrincipal in
-        fecha) cd comandos;
-        ./fecha.sh;;
-        infosystem) cd comandos;
-         ./infosystem.sh;;
-        arbol-ito) cd comandos;
-        ./arbolito.sh;;
-        prebe-player) cd PrebePlayer;
-        ./prebeplayer.sh;;
-        buscar) cd comandos;
-        ./buscar.sh;;
-        ayuda) cd comandos;
-        ./ayuda.sh;;
-        gatito) cd gato;
-        ./gato.sh;;
-        ahorcado) cd ahorcado;
-        ./ahor.sh;;
-        creditos) cd comandos;
-        ./creditos.sh;;
-        salir) exit;;
-        *) cd comandos;
-        ./invalida.sh;;
+    case $comando in
+        fecha) 
+            cd comandos;
+            ./fecha.sh
+        ;;
+        
+        infosystem) 
+            cd comandos;
+            ./infosystem.sh
+        ;;
+        
+        arbol-ito) 
+            cd comandos;
+            ./arbolito.sh
+        ;;
+        
+        prebe-player) 
+            cd PrebePlayer;
+            ./prebeplayer.sh
+        ;;
+        
+        buscar) 
+            cd comandos;
+            ./buscar.sh
+        ;;
+        
+        ayuda) 
+            cd comandos;
+            ./ayuda.sh
+        ;;
+        
+        gatito) 
+            cd gato;
+            ./gato.sh
+        ;;
+        
+        ahorcado) 
+            cd ahorcado;
+            ./ahor.sh
+        ;;
+        
+        creditos) 
+            cd comandos;
+            ./creditos.sh
+        ;;
+        
+        salir) 
+            exit
+        ;;
+        
+        *) 
+            $comando > /dev/null #salida.txt
+            if [[ $? -ne 0 ]]; then 
+                echo -e "\e[31m\tNo existe el comando. Intente de nuevo... \e[0m"
+                sleep 1
+                #rm salida.txt
+            else 
+                $comando
+                echo -e "\n\tPresione ENTER para continuar"
+                echo -ne "\t >> "
+                read enter
+            fi
+        ;;
     esac
     echo "";
 }
-MyShell
+
+exit_value=0
+while [ $exit_value -ne 1 ]
+do
+    MyShell
+done
